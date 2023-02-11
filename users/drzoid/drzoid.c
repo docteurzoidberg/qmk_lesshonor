77b7a5cceafdd1f285151e51bad8bdcff98169fb
DrZoid <docteurzoidberg@users.noreply.github.com>
2023-02-11 12:04:41 +0100

    ported xd75 to new qmk

---
+++ users/drzoid/drzoid.c
@@ -0,0 +1,25 @@
+#include "drzoid.h"
+
+void my_custom_function(void) {
+
+}
+
+void rgblight_setrgb_cyan() {
+    rgblight_sethsv(0xC3,  0xFF, 0xEE);
+}
+
+void rgblight_setrgb_orange() {
+    rgblight_sethsv(0x1E,  0xFF, 0xEE);
+}
+
+void rgblight_setrgb_red() {
+    rgblight_sethsv(0x00,  0xFF, 0xEE);
+}
+
+void rgblight_setrgb_yellow() {
+    rgblight_sethsv(0x3C,  0xFF, 0xEE);
+}
+
+void rgblight_setrgb_green() {
+    rgblight_sethsv(0x78,  0xFF, 0xEE);
+}
---
+++ users/drzoid/process_longpress.h
@@ -0,0 +1,44 @@
+/* Copyright 2017 Jeremy Cowgar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROCESS_LONGPRESS_H
+#define PROCESS_LONGPRESS_H
+
+#include "quantum.h"
+
+#ifndef LONGPRESS_TIMEOUT
+  #define LONGPRESS_TIMEOUT 175
+#endif
+
+bool process_longpress(uint16_t keycode, keyrecord_t *record);
+
+void longpress_increase_timeout(uint16_t amount);
+void longpress_decrease_timeout(uint16_t amount);
+void longpress_enable(void);
+void longpress_disable(void);
+void longpress_toggle(void);
+bool longpress_state(void);
+void longpress_timer_report(void);
+void longpress_on(uint16_t keycode);
+void longpress_flush(void);
+
+
+uint16_t longpress_time;
+uint16_t longpress_timeout;
+uint16_t longpress_lastkey;
+bool longpress_enabled;
+
+#endif
