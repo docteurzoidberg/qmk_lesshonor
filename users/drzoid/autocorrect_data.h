9a3a5501c32b9fe9b50c2d31430164401998e32c
DrZoid <docteurzoidberg@users.noreply.github.com>
2023-02-15 11:10:57 +0100

    drzoid user files > wip shared

---
+++ users/drzoid/autocorrect_data.h
@@ -0,0 +1,19 @@
+// Generated code.
+
+// Autocorrection dictionary (5 entries):
+//   :thier -> their
+//   fitler -> filter
+//   lenght -> length
+//   ouput  -> output
+//   widht  -> width
+
+#define AUTOCORRECT_MIN_LENGTH 5  // "ouput"
+#define AUTOCORRECT_MAX_LENGTH 6  // ":thier"
+
+#define DICTIONARY_SIZE 74
+
+static const uint8_t autocorrect_data[DICTIONARY_SIZE] PROGMEM = {85, 7, 0, 23, 35, 0, 0, 8, 0, 76, 16, 0, 15, 25, 0, 0,
+    11, 23, 44, 0, 130, 101, 105, 114, 0, 23, 12, 9, 0, 131, 108, 116, 101, 114, 0, 75, 42, 0, 24, 64, 0, 0, 71, 49, 0,
+    10, 56, 0, 0, 12, 26, 0, 129, 116, 104, 0, 17, 8, 15, 0, 129, 116, 104, 0, 19, 24, 18, 0, 130, 116, 112, 117, 116,
+    0};
+
--- users/drzoid/drzoid.h
+++ users/drzoid/drzoid.h
@@ -3,6 +3,26 @@
 
 #include "quantum.h"
 
+enum userspace_custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  FN,
+  L2,
+  L3,
+  DRZ_LOCK,
+  DRZ_SWAP_HANDS=SH_MON,
+  DRZ_LEFT_SHIFT_CAPSLOCK,
+  DRZ_RIGHT_SHIFT_CAPSLOCK,
+  DRZ_ACCENTS_TAPDANCE,
+  DRZ_EMOJIS_TAPDANCE,
+  DRZ_LPUP,
+  DRZ_LPDN,
+  DRZ_LPRP,
+  DRZ_LPTG,
+  DRZ_LPON,
+  DRZ_LPOFF,
+  NEW_SAFE_RANGE // use "NEWPLACEHOLDER for keymap specific codes
+};
+
 void my_custom_function(void);
 #ifdef RGBLIGHT_ENABLE
     void drz_rgblight_set_cyan(void);
