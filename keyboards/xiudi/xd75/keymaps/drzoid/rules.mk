89a133a809077d2e55365b72e1a1f3ee952bd208
DrZoid <docteurzoidberg@users.noreply.github.com>
2023-02-21 13:57:15 +0100

    space gain

--- keyboards/xiudi/xd75/keymaps/drzoid/rules.mk
+++ keyboards/xiudi/xd75/keymaps/drzoid/rules.mk
@@ -4,32 +4,35 @@
 #drz custom user files features
 DRZ_USE_LONGPRESS = yes
 DRZ_USE_MACROS = yes
-DRZ_USE_DYNAMICMACROS = no
-DRZ_USE_AUTOCORRECTION = no
+DRZ_USE_DYNAMICMACROS = yes
+DRZ_USE_AUTOCORRECTION = yes
 DRZ_USE_UNICODE = yes
 DRZ_USE_SECRETS = yes
 DRZ_USE_RGBLIGHT = yes
-DRZ_USE_WPM = no
+DRZ_USE_WPM = yes
 
 #qmk features
+COMBO_ENABLE = no
 SWAP_HANDS_ENABLE = yes
 LEADER_ENABLE = yes
-TAPDANCE_ENABLE = yes
+#TAPDANCE_ENABLE = yes
 EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
 KEY_LOCK_ENABLE = yes
 
 #board
-SLEEP_LED_ENABLE = yes       # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 BACKLIGHT_ENABLE = yes
 BACKLIGHT_LEVELS = 16
 
 #disable
+TAPDANCE_ENABLE = no
 POINTING_DEVICE_ENABLE = no
 MOUSEKEY_ENABLE = no         # Mouse keys(+4700)
 COMMAND_ENABLE = no          # Commands for debug and configuration
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
+MUSIC_ENABLE = no
 
 #free spaaaaaaace
 AVR_USE_MINIMAL_PRINTF = yes
